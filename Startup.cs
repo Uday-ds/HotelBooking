330c879143c0dd8a2cbeb74ad5fc62761db041b9
Uday-ds <udaydskotta40@gmail.com>
2025-03-20 14:56:14 +0530

    Modified swagger end point.

--- Startup.cs
+++ Startup.cs
@@ -66,7 +66,7 @@ namespace HotelBooking
             // Configure Swagger UI
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "HotelBooking API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "HotelBooking V1");
                 c.RoutePrefix = string.Empty; // Swagger will be accessible at root URL
             });
 
